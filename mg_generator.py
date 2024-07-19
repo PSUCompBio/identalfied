9370c64d84515da039a453781d7b58dfcbc92f41
Youssef Darwich <58274928+fridge-png@users.noreply.github.com>
2024-07-19 12:05:09 +0200

    fixed bug in mg_generator

--- mg_generator.py
+++ mg_generator.py
@@ -5,9 +5,9 @@ from heapq import heappop, heappush
 import numpy as np
 import ast
 
-controls_max = { "L1" : 2 , "L2" : 1, "L3" : 1 , "R1" : 2, "R2" : 2 , "R3" : 2 , "R4" : 2, "R5" : 2, "R6" : 2}
+controls_max = { "L1" : 2 , "L2" : 2, "L3" : 2 , "L4" : 2 , "L5" : 2, "L6" : 2 , "R1" : 2, "R2" : 2 , "R3" : 2 , "R4" : 2, "R5" : 2, "R6" : 2}
 
-controls_min = { "L1" : 1 , "L2" : 1, "L5" : 0 , "R1" : 1}
+controls_min = { "L1" : 0 , "L2" : 0, "L3" : 0 , "L4" : 0 , "L5" : 0, "L6" : 0 , "R1" : 2, "R2" : 0 , "R3" : 0 , "R4" : 0, "R5" : 0, "R6" : 0}
 
 thickness = 6
 
@@ -15,25 +15,18 @@ filename_centroids = "Outputs\\centroids.txt"
 filename_min = "Outputs\\min_coords.txt"
 filename_max = "Outputs\\max_coords.txt"
 
-coordinates = [[-18.989004 , 2.49134793 , 5.02635869], [21.37638531 ,-4.69348574 , 5.22460919],[-21.41151684 , -5.26406629 , 4.99220662], [-23.70476265 ,-15.38073274 , 4.15185814], [20.12315651 ,4.21101178 , 4.48912539], [ 24.3411898 , -14.65882079  ,4.37077542], [-17.26862539 , 9.57301484 , 4.751961  ], [-5.1401908 , 20.54918814 , 3.1695357 ], [12.8205849 , 16.57466938 , 3.03112211], [16.54583193 ,10.23664677 , 4.00893114], [ 4.93446551 ,20.88632488 , 3.06768471], [-13.39225487 , 16.15103723 ,  3.62514866]]
 coordinates = []
 with open(filename_centroids, 'r') as file:
     for line in file:
         array = ast.literal_eval(line.strip())
         coordinates.append(array)
 
-min_coords = [[-21.69279099 , 5.46345202 , 1.71738549], [25.81974983,-1.81901673,-0.15976323], [-25.91950226 , -2.50392636 , -0.63969668], [-29.25651296 , -14.92009322 , -3.03469221], [21.7255586 , 6.41146533 , 0.28532448], 
-[29.36602211 , -13.79758485 , -0.57500548], [-19.25904719 , 10.62123235 , 0.85620173], [-5.71570333,20.38642565,-3.22707589], [12.84471003,17.58736165,-1.80159585],[18.16371028,12.03306548,0.02026811],  [4.72471809,20.41322962,-3.71867347], [-13.43736744,16.86110814,-0.66337164]]
 min_coords = []
 with open(filename_min, 'r') as file:
     for line in file:
         array = ast.literal_eval(line.strip())
         min_coords.append(array)
 
-
-max_coords_2 = [[-17.02765528,0.40995704,6.43885358], [19.05027135,-4.44540167,7.32627312], [-19.02100817,-6.12751643,7.1202879,], 
-[-21.15375074,-17.48965327,7.24505822], [20.47983233,3.51608046,6.0270977,], 
-[21.737758,-15.86221822,7.32187017], [-17.16126442,11.1145649,6.52075768], [-5.46424802,21.62669182,6.35972738], [13.78541374,16.25265471,5.79531574],[17.70253372,10.3689909,6.15421502], [3.92034348,21.78133901,6.29381625], [-13.79625924,16.78437487,6.47573105]]
 max_coords = []
 with open(filename_max, 'r') as file:
     for line in file:
@@ -151,18 +144,26 @@ def make_increase_width():
             id = "L" + str(len(left_indices)-index)
         else:
             id = "R" + str( (index + 1) % len(left_indices))
-            
-        min = min_coords[index]
+        
+        coord_index = find_coordinate_index(vertex.co)
+        
+        min = min_coords[coord_index]
         if controls_min.get(id):
             min[2] -= controls_min.get(id)
-        max = max_coords[index]
+        min[0] = vertex.co[0]
+        min[1] = vertex.co[1]
+        
+        max = max_coords[coord_index]
         if controls_max.get(id):
             max[2] += controls_max.get(id)
+        max[0] = vertex.co[0]
+        max[1] = vertex.co[1]
         
-        delta_z = adjust_center_point(coordinates[index],min, max)
+        delta_z = adjust_center_point(vertex.co,min, max)
+        scale_size.append(equalize_distances(vertex.co,min, max))
+        
         vertex.co = (vertex.co[0], vertex.co[1], vertex.co[2] + delta_z)
-        
-        scale_size.append(equalize_distances(coordinates[index],min, max))    
+            
     
     for index,vertex in enumerate(obj.data.skin_vertices[""].data):
         vertex.radius = (thickness, scale_size[index])
@@ -171,17 +172,15 @@ def make_increase_width():
 def get_distance(point1 , point2):
     return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2 + (point2[2] - point1[2]) ** 2)
     
-def get_distances_between_coords(coords1, coords2):
-    coords = []
-    for i in range(len(coords2)):
-        coords.append(get_distance(coords1[i], coords2[i]))
-    return coords
 
 def adjust_center_point(centroid_point, min_point, max_point):
     # Calculate the required vertical movement
-    delta_z = (get_distance(centroid_point , max_point) - get_distance(centroid_point, min_point)) / 2
+    distance_max = get_distance(centroid_point , max_point)
+    distance_min = get_distance(centroid_point, min_point)
+    delta_z = (distance_max - distance_min) / 2
     
     return delta_z
+
 
 def equalize_distances(centroid_point, min_point, max_point):
     # Calculate the required vertical movement
@@ -191,12 +190,24 @@ def equalize_distances(centroid_point, min_point, max_point):
     
     return scale_size
 
-def equalize_minmax_z():
-    for index,vertex in enumerate(coordinates):
-        min_coords[index][0] = vertex[0]
-        min_coords[index][1] = vertex[1]
-        max_coords[index][0] = vertex[0]
-        max_coords[index][1] = vertex[1]
+def add_sphere_at(loc):
+    bpy.ops.mesh.primitive_ico_sphere_add(radius=1, enter_editmode=False, align='WORLD', location= loc, scale=(1, 1, 1))
+    
+
+def find_coordinate_index(target):
+    # Rounding function to 4 decimal places
+    def round_coord(coord):
+        return list(round(c, 4) for c in coord)
+    
+    # Round the target coordinate
+    rounded_target = round_coord(target)
+    
+    for index, coord in enumerate(coordinates):
+        if round_coord(coord) == rounded_target:
+            return index
+    
+    # If no match is found
+    return -1
 
 filePath = "E:\\Projects\\MouthGuard\\Models\\"
 modelName = "12 year old male.stl"
@@ -233,8 +244,6 @@ teeth_obj.select_set(False)
 
 print("Dental scan oriented correctly.")
 
-
-equalize_minmax_z()
 
 
 # Get the edges based on the minimum spanning tree
@@ -273,11 +282,10 @@ bpy.ops.object.convert(target='MESH')
 
 print("Fixed indices.")
 
-
 bpy.ops.object.modifier_add(type='SKIN')
-
 make_increase_width()
-
+bpy.context.view_layer.objects.active = obj
+obj.select_set(True)
 
 print("Added Thickness.")
 
